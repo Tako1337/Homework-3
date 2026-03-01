ab9b8e130687614e4b93a822aa75873926a1b0a6
Tako1337 <talgatbakyt2@gmail.com>
2026-03-01 18:07:49 +0500

    Implement EnemyCombatantAdapter

---
+++ src/com/narxoz/rpg/adapter/EnemyCombatantAdapter.java
@@ -0,0 +1,33 @@
+package com.narxoz.rpg.adapter;
+
+import com.narxoz.rpg.battle.Combatant;
+import com.narxoz.rpg.enemy.Enemy;
+
+public class EnemyCombatantAdapter implements Combatant {
+
+    private final Enemy enemy;
+
+    public EnemyCombatantAdapter(Enemy enemy) {
+        this.enemy = enemy;
+    }
+
+    @Override
+    public String getName() {
+        return enemy.getType();
+    }
+
+    @Override
+    public int getAttackPower() {
+        return enemy.getDamage();
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        enemy.applyDamage(damage);
+    }
+
+    @Override
+    public boolean isAlive() {
+        return enemy.isAlive();
+    }
+}
