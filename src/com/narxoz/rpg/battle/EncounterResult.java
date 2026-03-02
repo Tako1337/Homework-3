0d729c08d4d63599009d545568ad98508f7d48a4
Tako1337 <talgatbakyt2@gmail.com>
2026-03-02 13:33:50 +0500

    Add EncounterResult class for battle outcome representation

---
+++ src/com/narxoz/rpg/battle/EncounterResult.java
@@ -0,0 +1,13 @@
+package com.narxoz.rpg.battle;
+
+public class EncounterResult {
+    private final String winner;
+
+    public EncounterResult(String winner) {
+        this.winner = winner;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+}
